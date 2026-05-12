bdb38b93314b46b453587b277f0d29c05beab970
itxnaveenjs <itx.naveen.js@gmail.com>
2026-05-12 17:56:24 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showString("HELLO MICRO:BIT! ")
+})
+basic.forever(function () {
+	
+})
